97da348f0bcb8e75f1ca037294c2622fb8c8c102
mi nombre <desarrollowebaps@gmail.com>
2023-02-05 02:27:42 -0400

    11_digitalCode

--- js/templates/template_landing.js
+++ js/templates/template_landing.js
@@ -12,15 +12,27 @@ const dataFechWeb = async ()=>{
     try{
         const resWeb = await fetch("landing-templates.json")
         const dataWeb = await resWeb.json()
-        pintarlanding(dataWeb)
+        pintarAll(dataWeb)
 
         document.addEventListener("click", (e)=>{
             if(e.target.id === "lnum1"){
-                contTemplateWeb.textContent = pintarlanding(dataWeb)
-                pintarlanding(dataWeb)
+                contTemplateWeb.textContent = pintarAll(dataWeb)
+                pintarAll(dataWeb)
             }else if(e.target.id === "lnum2"){
-                contTemplateWeb.textContent = pintarlanding2(dataWeb)
-                pintarlanding2(dataWeb)
+                contTemplateWeb.textContent = pintarRestaurant(dataWeb)
+                pintarRestaurant(dataWeb)
+            }else if(e.target.id === "lnum3"){
+                contTemplateWeb.textContent = pintarPortfolios(dataWeb)
+                pintarPortfolios(dataWeb)
+            }else if(e.target.id === "lnum4"){
+                contTemplateWeb.textContent = pintarVeterinaria(dataWeb)
+                pintarVeterinaria(dataWeb)
+            }else if(e.target.id === "lnum5"){
+                contTemplateWeb.textContent = pintarGamer(dataWeb)
+                pintarGamer(dataWeb)
+            }else if(e.target.id === "lnum6"){
+                contTemplateWeb.textContent = pintarFashion(dataWeb)
+                pintarFashion(dataWeb)
             }
         })
         
@@ -31,8 +43,8 @@ const dataFechWeb = async ()=>{
 
 
 //FUNCION LANDING
-const pintarlanding = (dataWeb) =>{
-    dataWeb.landing.forEach(element => {
+const pintarAll = (dataWeb) =>{
+    dataWeb.all.forEach(element => {
         //console.log(element.image1)
         templateWeb.querySelector("#image1").setAttribute("src", element.image1)
         templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
@@ -89,6 +101,14 @@ const pintarlanding = (dataWeb) =>{
             window.open("https://antonmarchitec.github.io/landing_resposteria/")
         }else if(e.target.dataset.id === "4"){
             window.open("https://antonmarchitec.github.io/template_gamer/")
+        }else if(e.target.dataset.id === "5"){
+            window.open("https://antonmarchitec.github.io/template_visual_designer/")
+        }else if(e.target.dataset.id === "6"){
+            window.open("https://antonmarchitec.github.io/template_visit-tokio/")
+        }else if(e.target.dataset.id === "7"){
+            window.open("https://antonmarchitec.github.io/template_halloween/")
+        }else if(e.target.dataset.id === "8"){
+            window.open("https://antonmarchitec.github.io/fashion__template/")
         }
         
     }) 
@@ -96,8 +116,59 @@ const pintarlanding = (dataWeb) =>{
 
 
 //FUNCION LANDING 2
-const pintarlanding2 = (dataWeb) =>{
-    dataWeb.landing2.forEach(element => {
+const pintarRestaurant = (dataWeb) =>{
+    dataWeb.restaurant.forEach(element => {
+        //console.log(element.image1)
+        templateWeb.querySelector("#image1").setAttribute("src", element.image1)
+        templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
+        templateWeb.querySelector("#image2").setAttribute("src", element.image2)
+        templateWeb.querySelector("#image2").setAttribute("alt", element.title2)
+
+        
+        templateWeb.querySelector("#webTitle").textContent = element.title1
+        templateWeb.querySelector("#webTitle2").textContent = element.title2
+
+        templateWeb.querySelector("#webDescription").textContent = element.descripcion1
+        templateWeb.querySelector("#webDescription2").textContent = element.descripcion2
+
+        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
+        templateWeb.querySelector("#titleResp").textContent = element.subtitleResp
+        templateWeb.querySelector("#titleResp2").textContent = element.subtitleResp
+
+        templateWeb.querySelector("#desktop").textContent = element.dis1
+        templateWeb.querySelector("#tablet").textContent = element.dis2
+        templateWeb.querySelector("#movil").textContent = element.dis3
+        templateWeb.querySelector("#desktop2").textContent = element.dis1
+        templateWeb.querySelector("#tablet2").textContent = element.dis2
+        templateWeb.querySelector("#movil2").textContent = element.dis3
+
+        templateWeb.querySelector("#text1A").textContent = element.tip1A
+        templateWeb.querySelector("#text1B").textContent = element.tip1B
+        templateWeb.querySelector("#text1C").textContent = element.tip1C
+        templateWeb.querySelector("#text1D").textContent = element.tip1D
+
+        templateWeb.querySelector("#text2A").textContent = element.tip1A
+        templateWeb.querySelector("#text2B").textContent = element.tip1B
+        templateWeb.querySelector("#text2C").textContent = element.tip1C
+        templateWeb.querySelector("#text2D").textContent = element.tip2D
+
+
+        templateWeb.querySelector("#btnWeb1").dataset.id = element.id_block1
+        templateWeb.querySelector("#btnWeb2").dataset.id = element.id_block2
+
+
+        let cloneWeb = templateWeb.cloneNode(true)
+        fragmentWeb.appendChild(cloneWeb)
+    });
+    contTemplateWeb.appendChild(fragmentWeb)
+   
+}
+
+
+
+//FUNCION LANDING 2
+const pintarPortfolios = (dataWeb) =>{
+    dataWeb.portfolios.forEach(element => {
         //console.log(element.image1)
         templateWeb.querySelector("#image1").setAttribute("src", element.image1)
         templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
@@ -144,14 +215,161 @@ const pintarlanding2 = (dataWeb) =>{
    
 
 
-    //Ejecutar eventos
-    document.addEventListener("click", (e)=>{
-        if(e.target.dataset.id === "5"){
-            window.open("https://antonmarchitec.github.io/template_visual_designer/")
-        }else if(e.target.dataset.id === "6"){
-            window.open("https://antonmarchitec.github.io/template_visit-tokio/")
-        }else if(e.target.dataset.id === "7"){
-            window.open("https://antonmarchitec.github.io/template_halloween/")
-        }
-    }) 
+  
+}
+
+
+//FUNCION LANDING 2
+const pintarVeterinaria = (dataWeb) =>{
+    dataWeb.veterinaria.forEach(element => {
+        //console.log(element.image1)
+        templateWeb.querySelector("#image1").setAttribute("src", element.image1)
+        templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
+        templateWeb.querySelector("#image2").setAttribute("src", element.image2)
+        templateWeb.querySelector("#image2").setAttribute("alt", element.title2)
+
+        
+        templateWeb.querySelector("#webTitle").textContent = element.title1
+        templateWeb.querySelector("#webTitle2").textContent = element.title2
+
+        templateWeb.querySelector("#webDescription").textContent = element.descripcion1
+        templateWeb.querySelector("#webDescription2").textContent = element.descripcion2
+
+        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
+        templateWeb.querySelector("#titleResp").textContent = element.subtitleResp
+        templateWeb.querySelector("#titleResp2").textContent = element.subtitleResp
+
+        templateWeb.querySelector("#desktop").textContent = element.dis1
+        templateWeb.querySelector("#tablet").textContent = element.dis2
+        templateWeb.querySelector("#movil").textContent = element.dis3
+        templateWeb.querySelector("#desktop2").textContent = element.dis1
+        templateWeb.querySelector("#tablet2").textContent = element.dis2
+        templateWeb.querySelector("#movil2").textContent = element.dis3
+
+        templateWeb.querySelector("#text1A").textContent = element.tip1A
+        templateWeb.querySelector("#text1B").textContent = element.tip1B
+        templateWeb.querySelector("#text1C").textContent = element.tip1C
+        templateWeb.querySelector("#text1D").textContent = element.tip1D
+
+        templateWeb.querySelector("#text2A").textContent = element.tip1A
+        templateWeb.querySelector("#text2B").textContent = element.tip1B
+        templateWeb.querySelector("#text2C").textContent = element.tip1C
+        templateWeb.querySelector("#text2D").textContent = element.tip2D
+
+
+        templateWeb.querySelector("#btnWeb1").dataset.id = element.id_block1
+        templateWeb.querySelector("#btnWeb2").dataset.id = element.id_block2
+
+
+        let cloneWeb = templateWeb.cloneNode(true)
+        fragmentWeb.appendChild(cloneWeb)
+    });
+    contTemplateWeb.appendChild(fragmentWeb)
+   
+
+
+   //Ejecutar eventos
+   document.addEventListener("click", (e)=>{
+   
+    
+}) 
+}
+
+
+
+//FUNCION LANDING 2
+const pintarGamer = (dataWeb) =>{
+    dataWeb.gamer.forEach(element => {
+        //console.log(element.image1)
+        templateWeb.querySelector("#image1").setAttribute("src", element.image1)
+        templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
+        templateWeb.querySelector("#image2").setAttribute("src", element.image2)
+        templateWeb.querySelector("#image2").setAttribute("alt", element.title2)
+
+        
+        templateWeb.querySelector("#webTitle").textContent = element.title1
+        templateWeb.querySelector("#webTitle2").textContent = element.title2
+
+        templateWeb.querySelector("#webDescription").textContent = element.descripcion1
+        templateWeb.querySelector("#webDescription2").textContent = element.descripcion2
+
+        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
+        templateWeb.querySelector("#titleResp").textContent = element.subtitleResp
+        templateWeb.querySelector("#titleResp2").textContent = element.subtitleResp
+
+        templateWeb.querySelector("#desktop").textContent = element.dis1
+        templateWeb.querySelector("#tablet").textContent = element.dis2
+        templateWeb.querySelector("#movil").textContent = element.dis3
+        templateWeb.querySelector("#desktop2").textContent = element.dis1
+        templateWeb.querySelector("#tablet2").textContent = element.dis2
+        templateWeb.querySelector("#movil2").textContent = element.dis3
+
+        templateWeb.querySelector("#text1A").textContent = element.tip1A
+        templateWeb.querySelector("#text1B").textContent = element.tip1B
+        templateWeb.querySelector("#text1C").textContent = element.tip1C
+        templateWeb.querySelector("#text1D").textContent = element.tip1D
+
+        templateWeb.querySelector("#text2A").textContent = element.tip1A
+        templateWeb.querySelector("#text2B").textContent = element.tip1B
+        templateWeb.querySelector("#text2C").textContent = element.tip1C
+        templateWeb.querySelector("#text2D").textContent = element.tip2D
+
+
+        templateWeb.querySelector("#btnWeb1").dataset.id = element.id_block1
+        templateWeb.querySelector("#btnWeb2").dataset.id = element.id_block2
+
+
+        let cloneWeb = templateWeb.cloneNode(true)
+        fragmentWeb.appendChild(cloneWeb)
+    });
+    contTemplateWeb.appendChild(fragmentWeb)
+}
+
+
+//FUNCION LANDING 2
+const pintarFashion = (dataWeb) =>{
+    dataWeb.fashion.forEach(element => {
+        //console.log(element.image1)
+        templateWeb.querySelector("#image1").setAttribute("src", element.image1)
+        templateWeb.querySelector("#image1").setAttribute("alt", element.title1)
+        templateWeb.querySelector("#image2").setAttribute("src", element.image2)
+        templateWeb.querySelector("#image2").setAttribute("alt", element.title2)
+
+        
+        templateWeb.querySelector("#webTitle").textContent = element.title1
+        templateWeb.querySelector("#webTitle2").textContent = element.title2
+
+        templateWeb.querySelector("#webDescription").textContent = element.descripcion1
+        templateWeb.querySelector("#webDescription2").textContent = element.descripcion2
+
+        templateWeb.querySelector("#titleProy").textContent = element.subtitleProy
+        templateWeb.querySelector("#titleResp").textContent = element.subtitleResp
+        templateWeb.querySelector("#titleResp2").textContent = element.subtitleResp
+
+        templateWeb.querySelector("#desktop").textContent = element.dis1
+        templateWeb.querySelector("#tablet").textContent = element.dis2
+        templateWeb.querySelector("#movil").textContent = element.dis3
+        templateWeb.querySelector("#desktop2").textContent = element.dis1
+        templateWeb.querySelector("#tablet2").textContent = element.dis2
+        templateWeb.querySelector("#movil2").textContent = element.dis3
+
+        templateWeb.querySelector("#text1A").textContent = element.tip1A
+        templateWeb.querySelector("#text1B").textContent = element.tip1B
+        templateWeb.querySelector("#text1C").textContent = element.tip1C
+        templateWeb.querySelector("#text1D").textContent = element.tip1D
+
+        templateWeb.querySelector("#text2A").textContent = element.tip1A
+        templateWeb.querySelector("#text2B").textContent = element.tip1B
+        templateWeb.querySelector("#text2C").textContent = element.tip1C
+        templateWeb.querySelector("#text2D").textContent = element.tip2D
+
+
+        templateWeb.querySelector("#btnWeb1").dataset.id = element.id_block1
+        templateWeb.querySelector("#btnWeb2").dataset.id = element.id_block2
+
+
+        let cloneWeb = templateWeb.cloneNode(true)
+        fragmentWeb.appendChild(cloneWeb)
+    });
+    contTemplateWeb.appendChild(fragmentWeb)
 }
